5fd70ab514fb8d521ad038f622e5b65164e6b69f
OverlordsIII <shrihunreddy@outlook.com>
2024-06-18 15:58:29 -0700

    update mod metadata

---
+++ src/client/java/io/github/overlordsiii/BalancedDietClient.java
@@ -0,0 +1,13 @@
+package io.github.overlordsiii;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BalancedDietClient implements ClientModInitializer {
+	/**
+	 * Runs the mod initializer on the client environment.
+	 */
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/io/github/overlordsiii/BalancedDiet.java
@@ -0,0 +1,13 @@
+package io.github.overlordsiii;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BalancedDiet implements ModInitializer {
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize() {
+
+	}
+}
